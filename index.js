692a489788da4bffa681ce0f6a9cbb8d4e0b4d22
Jesse <jesse.marr@cewa.edu.au>
2021-04-01 09:58:46 +0800

    Update index.js
    Port 80 change

--- index.js
+++ index.js
@@ -135,4 +135,4 @@ app.post('/content/StaffRoomPres/RemoveOldEntries.html', async (request, respons
 
 
 
-app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
+app.listen(process.env.PORT || 80, () => console.log(`App available on http://localhost:3000`))
