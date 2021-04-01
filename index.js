3746051bd979268ca1f9068a2fcc84c530ffcb1b
Jesse <jesse.marr@cewa.edu.au>
2021-04-01 10:30:27 +0800

    Deleted Expired Messages

--- index.js
+++ index.js
@@ -9,11 +9,44 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+function isDateBeforeToday(date) {
+  return new Date(date.toDateString()) < new Date(new Date().toDateString());
+}
+
 
 // Default redirect
 app.get('/', async (request, response) => {
 
-    response.send( await readFile('./content/StaffRoomPres/index.html', 'utf8') );
+    
+  // Remove old entries
+  fs.readFile('content/StaffRoomPres/StaffRoomPres.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+
+    var i = 0;
+    obj.forEach(async function(element) {
+      
+      
+      if ((isDateBeforeToday(new Date(element.ExpiryDate))) || (element.Title == "")) {
+        removed = obj.splice(i,1);
+
+        var strNotes = JSON.stringify(obj);
+            fs.writeFile('content/StaffRoomPres/StaffRoomPres.json',strNotes, function(err){
+                if(err) return console.log(err);
+                console.log('Note deleted');
+            });
+      } else {
+        i++;
+      }
+    })
+
+
+  }});
+  
+  
+  response.send( await readFile('./content/StaffRoomPres/index.html', 'utf8') );
 
 });
 
@@ -65,7 +98,7 @@ app.post('/content/StaffRoomPres/AddNewEntries.html', async (request, response) 
     data: []
  };
 
- var newEntry = {"Title": request.body.Title, "Message":request.body.Message};
+ var newEntry = {"Title": request.body.Title, "Message":request.body.Message, "ExpiryDate":request.body.ExpiryDate};
 
   fs.readFile('content/StaffRoomPres/StaffRoomPres.json', 'utf8', function readFileCallback(err, data){
       if (err){
@@ -135,4 +168,4 @@ app.post('/content/StaffRoomPres/RemoveOldEntries.html', async (request, respons
 
 
 
-app.listen(process.env.PORT || 80, () => console.log(`App available on http://localhost:3000`))
+app.listen(process.env.PORT || 80, () => console.log(`App available on http://localhost:80`))
